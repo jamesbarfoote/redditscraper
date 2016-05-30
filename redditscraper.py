9a5debcc42e3498a2515ddbaf388a50a55dfc380
James Barfoote <james.barfoote@gmail.com>
2016-05-30 13:39:14 +1200

    added loop and list so that we can add as many sub reddits as we want without having to duplicate code

--- redditscraper.py
+++ redditscraper.py
@@ -3,6 +3,11 @@ import os
 import urllib.request
 from os import path,system,getenv
 
+#list to store the names of the subreddits to download from
+reddits = []
+reddits.append('earthporn') #picutes of earth / scenery
+reddits.append('NIotD') #NASA image of the day
+
 #check if the directory exists, if not then create it
 directory = 'wallpaper'
 if not os.path.exists(directory):
@@ -19,18 +24,19 @@ for i in os.listdir(os.getcwd()):
         print ('file is older than a week' + i)
         os.remove(i)
 
-r = praw.Reddit('Simple reddit scraper for /r/earthporn')
+r = praw.Reddit('Simple reddit scraper for reddit')
+for i in range(len(reddits)):
+    print (reddits[i])
+    for submission in r.get_subreddit(reddits[i]).get_new(limit=10):
+            extension = submission.url[-4:]
+            if os.path.isfile(path.basename(submission.url)) == False:
+                        if extension == '.jpg' or extension == '.png' or extension == '.gif':
+                                        print (submission.url)
+                                        urllib.request.urlretrieve(submission.url, path.basename(submission.url))
 
-for submission in r.get_subreddit('NIotD').get_new(limit=200):
-    extension = submission.url[-4:]
-    if os.path.isfile(path.basename(submission.url)) == False:
-        if extension == '.jpg' or extension == '.png' or extension == '.gif':
-            print (submission.url)
-            urllib.request.urlretrieve(submission.url, path.basename(submission.url))
-
-for submission in r.get_subreddit('earthporn').get_new(limit=200):
-    extension = submission.url[-4:]
-    if os.path.isfile(path.basename(submission.url)) == False:
-        if extension == '.jpg' or extension == '.png' or extension == '.gif':
-            print (submission.url)
-            urllib.request.urlretrieve(submission.url, path.basename(submission.url))
+# for submission in r.get_subreddit('earthporn').get_new(limit=200):
+#     extension = submission.url[-4:]
+#     if os.path.isfile(path.basename(submission.url)) == False:
+#         if extension == '.jpg' or extension == '.png' or extension == '.gif':
+#             print (submission.url)
+#             urllib.request.urlretrieve(submission.url, path.basename(submission.url))
